f3758590e1161d1b9ddf04875a4dae429869dbfc
RamonRomeroTec <A01700318@itesm.mx>
2018-09-24 03:02:08 -0500

    Imagenes

---
+++ image-fork-join-docs/secuential/ImageFrame.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------
+
+*
+
+* Actividad de programación: Fork-join framework
+
+* Fecha: 23-Sep-2018
+
+* Autor: A01700318 Ramon Romero
+
+*
+
+*--------------------------------------------------------------*/
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ImageFrame {
+
+	public static void showImage(String text, Image image) {
+        JFrame frame = new JFrame(text);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
